2ee597305807f778277b90e4bb0a37761e57fefd
SARANYA <SARANYA@192.168.1.36>
2024-05-19 14:03:38 +0530

    Initial commit

--- Letcode/src/main/java/hello/HelloWorld.java
+++ Letcode/src/main/java/hello/HelloWorld.java
@@ -2,6 +2,7 @@ package hello;
 
 //import hello.Practice_1; 
 
+
 public class HelloWorld {
    public static void main(String[] args) {     //Hello World          
 	System.out.println("HelloWorld");
